196461d1b182fd7be090fc05cbe38e736f199c99
leizakaryah <72323308+leizakaryah@users.noreply.github.com>
2020-10-05 09:46:51 -0700

    Add files via upload

---
+++ asawanimarie.py
@@ -0,0 +1,6 @@
+print "asawa ni marie"
+
+print "asawa", 1 + 2 +3
+print "marie", 1
+
+print "ang dami"
---
+++ ex2.py
@@ -0,0 +1,9 @@
+#Comment
+#comment ulit
+
+print "print naman" #comment pa ulit
+
+#dami comment 
+# pwede daw lagyan hashtag para madisable yung code
+
+print "pero di ko dinisable"
